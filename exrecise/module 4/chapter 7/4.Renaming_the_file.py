384e2abb8463208786ad93c56232991b95e1f17f
Prajwl-hash <prajwlram@gmail.com>
2024-12-09 22:35:01 +0530

    Add initial project files

--- exrecise/module 4/chapter 7/4.Renaming_the_file.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# Create a sample file to rename
-with open ('original_file.text','w') as f :
-    f.write('This file will be renamed.')
-
-# Rename the file
-os.rename('original_file.text','new_file.txt')
-print('File renamed from "original_file.text" to "new_file.txt"')
--- exrecise/module3/chapter 6/6.Generating_Quiz.py
+++ exrecise/module3/chapter 6/6.Generating_Quiz.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import random
 questions = [
     ("What is the capital of France?","paris"),
@@ -11,18 +10,4 @@ random.shuffle(questions)
 
 with open('quiz.txt','w') as file:
     for question,answer in questions:
-=======
-import random
-questions = [
-    ("What is the capital of France?","paris"),
-    ("What is 2 + 2 ","4"),
-    ("What is the color of the sky?","Blue"),
-]
-
-random.shuffle(questions)
-
-
-with open('quiz.txt','w') as file:
-    for question,answer in questions:
->>>>>>> c965dbf5152de176c2dbd9448cc71dbd01538425
         file.write(f"{question} (Answer :{answer})\n")
