a112374bdbcb58ea92120ed2fc4a05147b3b5875
Prajwl-hash <prajwlram@gmail.com>
2024-12-10 13:07:48 +0530

    Resolve merge conflicts

--- exrecise/module3/chapter 6/3.FIle_Reading.py
+++ exrecise/module3/chapter 6/3.FIle_Reading.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-with open ('input.txt','r') as infile, open ('output.txt','w') as outfile:
+with open('input.txt', 'r') as infile, open('output.txt', 'w') as outfile:
     for line in infile:
-=======
-with open ('input.txt','r') as infile, open ('output.txt','w') as outfile:
-    for line in infile:
->>>>>>> c965dbf5152de176c2dbd9448cc71dbd01538425
         outfile.write(line)
+
